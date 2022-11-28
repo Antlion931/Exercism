8e547dd68a7162dda9f638fd30611c2578645a1f
Antlion931 <AlbertK931@protonmail.com>
2022-11-28 20:24:52 +0100

    Grains completed

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn is_leap_year(year: u64) -> bool {
     match year {
         x if x % 400 == 0 => true,
-        x if x%4 == 0 && x%100 != 0 => true,
-        _ => false
+        x if x % 4 == 0 && x % 100 != 0 => true,
+        _ => false,
     }
 }

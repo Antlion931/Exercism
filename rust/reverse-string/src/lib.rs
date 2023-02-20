4f4bec5b9f9b827e44ab28d0a92f61dfe5fc5dad
Antlion931 <AlbertK931@protonmail.com>
2023-02-20 14:02:04 +0100

    Reverse string refactored

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let mut chars: Vec<_> = UnicodeSegmentation::graphemes(input, true).collect();
-    chars.reverse();
-    chars.into_iter().collect()
+    input.graphemes(true).rev().collect()
 }

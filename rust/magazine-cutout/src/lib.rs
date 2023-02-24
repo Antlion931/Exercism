e81f4a54c4928860fd326446245b093c90c7de96
Antlion931 <AlbertK931@protonmail.com>
2023-02-25 00:19:33 +0100

    Magazine cutout refactored

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -1,17 +1,10 @@
 use itertools::Itertools;
-use std::collections::HashMap;
-
-fn count_words<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
-    itertools::sorted(words.iter())
-            .dedup_with_count()
-            .map(|(n, w)| (*w, n)).collect::<HashMap<_, _>>()
-}
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    let magazine = count_words(magazine);
+    let magazine = magazine.iter().counts();
 
-    for (word, note_count) in count_words(note) {
-        if magazine.get(word).filter(|magazine_count| **magazine_count >= note_count).is_none() {
+    for (word, note_count) in note.iter().counts() {
+        if magazine.get(&word).filter(|magazine_count| **magazine_count >= note_count).is_none() {
             return false;
         }
     }

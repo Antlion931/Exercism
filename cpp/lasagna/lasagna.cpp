0a458b2a07b86ef361df2b24ef32d2d3f0c0207e
Antlion931 <AlbertK931@protonmail.com>
2024-06-28 12:37:13 +0200

    Cpp: lasagna completed

---
+++ cpp/lasagna/lasagna.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+// ovenTime returns the amount in minutes that the lasagna should stay in the
+// oven.
+int ovenTime() {
+    return 40;
+}
+
+/* remainingOvenTime returns the remaining
+   minutes based on the actual minutes already in the oven.
+*/
+int remainingOvenTime(int actualMinutesInOven) {
+    return std::max(ovenTime() - actualMinutesInOven, 0);
+}
+
+/* preparationTime returns an estimate of the preparation time based on the
+   number of layers and the necessary time per layer.
+*/
+int preparationTime(int numberOfLayers) {
+    return std::max(numberOfLayers * 2, 0);
+}
+
+// elapsedTime calculates the total time spent to create and bake the lasagna so
+// far.
+int elapsedTime(int numberOfLayers, int actualMinutesInOven) {
+    return preparationTime(numberOfLayers) + std::max(actualMinutesInOven, 0);
+}
